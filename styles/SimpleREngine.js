4943bfdd3c39ff5e9e9a2aef64d4cf1b29d0d644
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2021-05-10 00:30:31 +0200

    test77777

--- styles/SimpleREngine.js
+++ styles/SimpleREngine.js
@@ -26,3 +26,14 @@ function openPage(evt, cityName) {
   evt.currentTarget.className += " active";
 }
 
+window.onload = function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("xml_http_REquest").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://7r.re/styles/footer.html", true);
+  xhttp.send();
+}

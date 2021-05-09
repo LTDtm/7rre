6171df45181a117ec23c3d999f845720f3e15c92
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2021-05-09 14:14:12 +0200

    77777js

--- styles/SimpleREngine.js
+++ styles/SimpleREngine.js
@@ -1,6 +1,5 @@
-object.onload = function()
-{
-	function loadDoc() {
+object.onload = function(){loadDoc()};
+function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -8,7 +7,6 @@ object.onload = function()
       this.responseText;
     }
   };
-  xhttp.open("GET", "https://7r.re/styles/footer/", true);
+  xhttp.open("GET", "https://7r.re/styles/footer.html", true);
   xhttp.send();
 }
-};

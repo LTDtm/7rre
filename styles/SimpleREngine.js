e01f2780204fccdaba7d57fae34f4b976150e729
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2021-05-10 00:13:05 +0200

    77777777777re

--- styles/SimpleREngine.js
+++ styles/SimpleREngine.js
@@ -1,13 +1,31 @@
-
-function loadDoc() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("xml_http_REquest").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "https://7r.re/styles/footer.html", true);
-  xhttp.send();
+window.onload = function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
 
+window.onload = function openPage(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("pagecontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("pagelink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="PageDefaultOpen" and click on it
+document.getElementById("PageDefaultOpen").click();

07d1c7b515bc7e7c7d3e4ec408758221bf46ff50
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2021-05-09 14:11:01 +0200

    77777777777

---
+++ styles/SimpleREngine.js
@@ -0,0 +1,14 @@
+object.onload = function()
+{
+	function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("xml_http_REquest").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "https://7r.re/styles/footer/", true);
+  xhttp.send();
+}
+};

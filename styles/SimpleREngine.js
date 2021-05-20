8ae627bdcccf1475c433656c69127a647d191135
ErtY'wek <34419000+ErtYwek@users.noreply.github.com>
2021-05-20 19:13:23 +0200

    777777777

--- styles/SimpleREngine.js
+++ styles/SimpleREngine.js
@@ -31,7 +31,7 @@ window.onload = function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("xmlhttp_footer1").innerHTML =
+      document.getElementById("xmlhttp_foot").innerHTML =
       this.responseText;
     }
   };
